1008210f5a3982ebc6e49e228d937abb9f8d8843
Tigist Wondimneh <wondimnehtigist@gmail.com>
2023-04-23 11:09:20 +0300

    add team controller

---
+++ first_dotnet_proj/Controllers/TeamsController.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace first_dotnet_project.Controllers;
+
+[Route(template: "api/[controller]")]
+[ApiController]
+public class TeamsController : ControllerBase
+{
+
+}
