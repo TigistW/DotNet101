741e0722eeb93d9256a08cd4fda5b9ce6a25fa6f
Tigist Wondimneh <wondimnehtigist@gmail.com>
2023-04-24 18:48:44 +0300

    final fixes on routes

---
+++ first_dotnet_proj/Controllers/TeamsController.cs
@@ -0,0 +1,61 @@
+using first_dotnet_proj.Data;
+using first_dotnet_proj.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace first_dotnet_project.Controllers;
+
+[Route(template: "api/[controller]")]
+[ApiController]
+public class teamsController : ControllerBase
+{
+
+
+    private static AppDbContext _context;
+    public teamsController(AppDbContext context){
+        _context = context;
+    }
+    [HttpGet]
+    public async Task<IActionResult> Get()
+    {
+        var teams = await _context.teams.ToListAsync();
+        return Ok(teams);
+    }
+
+    [HttpGet(template:"{id:int}")]
+    public async Task<IActionResult> Get(int id){
+    
+        var team = await _context.teams.FirstOrDefaultAsync(x => x.Id == id);
+        if (team ==null)
+            return BadRequest(error:"Invalid id");
+        
+        return Ok(team);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Post(Team team){
+        await _context.teams.AddAsync(team);
+        await _context.SaveChangesAsync();
+        return CreatedAtAction("Get", routeValues:team.Id, value:team);
+    }
+
+    [HttpPatch]
+    public async Task<IActionResult> Patch (int id, string country){
+        var team = await _context.teams.FirstOrDefaultAsync(x => x.Id ==id);
+        if (team == null)
+            return BadRequest(error:"Invalid Id");
+        team.Country = country;
+        return NoContent();
+    }
+
+    [HttpDelete]
+    public async Task<IActionResult> Delete(int id)
+    {
+        var team = await _context.teams.FirstOrDefaultAsync(x => x.Id == id);
+        if (team == null)
+            return BadRequest(error: "Invalid Id");
+        _context.teams.Remove(team);
+        await _context.SaveChangesAsync();
+        return NoContent();
+    }
+}
---
+++ first_dotnet_proj/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+using first_dotnet_proj.Models;
+using Microsoft.EntityFrameworkCore;
+namespace first_dotnet_proj.Data;
+
+public class AppDbContext : DbContext {
+  
+
+    public DbSet<Team> teams {get;set;}
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+
+    } 
+    
+
+}
