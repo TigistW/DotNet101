36bb28958306d622acd8e4bdad63a8c9ec037742
Tigist Wondimneh <wondimnehtigist@gmail.com>
2023-04-24 11:30:11 +0300

    add app db and http methods

--- first_dotnet_proj/Controllers/TeamsController.cs
+++ first_dotnet_proj/Controllers/TeamsController.cs
@@ -1,3 +1,5 @@
+using first_dotnet_proj.Data;
+using first_dotnet_proj.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace first_dotnet_project.Controllers;
@@ -7,4 +9,70 @@ namespace first_dotnet_project.Controllers;
 public class TeamsController : ControllerBase
 {
 
+
+    private static AppDbContext _context;
+    public TeamsController(AppDbContext context){
+        _context = context;
+    }
+    private static List<Team> teams = new List<Team>(){
+        new Team(){
+            Country = "Germany",
+            Name = "Honda",
+            Id = 1,
+            TeamPrinciple = "kelel"
+        },
+        new Team(){
+            Country = "France",
+            Name = "Honda",
+            Id = 2,
+            TeamPrinciple = "kelel"
+        },
+        new Team(){
+            Country = "Spain",
+            Name = "Honda",
+            Id = 3,
+            TeamPrinciple = "kelel"
+        }
+    };
+
+    [HttpGet]
+    public IActionResult Get()
+    {
+        return Ok(teams);
+    }
+
+    [HttpGet(template:"{id:int}")]
+    public IActionResult Get(int id){
+    
+        var team = teams.FirstOrDefault(x => x.Id == id);
+        if (team ==null)
+            return BadRequest(error:"Invalid id");
+        
+        return Ok(team);
+    }
+
+    [HttpPost]
+    public IActionResult Post(Team team){
+        teams.Add(team);
+        return CreatedAtAction("Get", routeValues:team.Id, value:team);
+    }
+
+    [HttpPatch]
+    public IActionResult Patch (int id, string country){
+        var team = teams.FirstOrDefault(x => x.Id ==id);
+        if (team == null)
+            return BadRequest(error:"Invalid Id");
+        team.Country = country;
+        return NoContent();
+    }
+
+    [HttpDelete]
+    public IActionResult Delete(int id)
+    {
+        var team = teams.FirstOrDefault(x => x.Id == id);
+        if (team == null)
+            return BadRequest(error: "Invalid Id");
+        teams.Remove(team);
+        return NoContent();
+    }
 }
--- first_dotnet_proj/Program.cs
+++ first_dotnet_proj/Program.cs
@@ -1,3 +1,6 @@
+using first_dotnet_proj.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,6 +10,8 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddDbContext<AppDbContext>(options => 
+options.UseSqlite(builder.Configuration.GetConnectionString(name:"DefaultConnection")));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
